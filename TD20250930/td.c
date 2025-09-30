bdf6cf06975a8c550eb703ce103e490d9c6e95e8
CottetMathis <mathis.cottet@heig-vd.ch>
2025-09-30 12:32:48 +0000

    added TD20250930

---
+++ TD20250930/td.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+
+	const double G = 6.67E-11;
+	double m1 = 0;
+	double m2 = 0;
+	double dist = 0;
+
+	printf("Calculateur de force gravitationnelle \n Masse 1 [kg]:");
+
+	scanf("%lf", &m1);
+	
+
+}
