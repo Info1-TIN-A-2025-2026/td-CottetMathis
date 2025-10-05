332cb017f87568d5f46642bbc996f9af5d91e510
CottetMathis <mathis.cottet@heig-vd.ch>
2025-10-05 13:21:00 +0000

    TD20250930 fin de programme

--- TD20250930/td.c
+++ TD20250930/td.c
@@ -7,10 +7,37 @@ int main(int argc, const char *argv[])
 	double m1 = 0;
 	double m2 = 0;
 	double dist = 0;
+	double fg = 0;
 
-	printf("Calculateur de force gravitationnelle \n Masse 1 [kg]:");
+	printf("Calculateur de force gravitationnelle \nEntrer la masse 1 [kg]: ");
 
 	scanf("%lf", &m1);
-	
 
+	if (m1 <= 0)
+	{
+		printf("Erreur, m1 ne peut ne peut pas être égale ou inférieur à 0");
+	}
+
+	printf("Entrer la masse 2 [kg]: ");
+
+	scanf("%lf", &m2);
+
+	if (m2 < 0)
+	{
+		printf("Erreur, m2 ne peut ne peut pas être égale ou inférieur à 0");
+	}
+
+	printf("Entrer la distance entre les 2 corps [m]: ");
+
+	scanf("%lf", &dist);
+
+	if (dist < 0)
+	{
+		printf("Erreur, la distance ne peut ne peut pas être égale ou inférieur à 0");
+	}
+
+
+	fg = G * (m1 * m2) / (dist * dist);
+
+	printf("F= %e N \n", fg);
 }
